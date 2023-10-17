bcae944bb44a91152660a70bbf59d0a471bf135b
tomascanna <tomas742011@gmail.com>
2023-10-17 16:08:49 -0300

    add: interfaz grafica

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import PySimpleGUI as sg
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -8,7 +9,7 @@ with open('data.json', encoding='utf-8') as f:
     data = json.load(f)
 
 # Extraer preguntas y respuestas del conjunto de datos
-preguntas = [item["pregunta"] for item in data["data"]]
+preguntas  = [item["pregunta"] for item in data["data"]]
 respuestas = [item["respuesta"] for item in data["data"]]
 
 # Crear un modelo de clasificación
@@ -21,31 +22,64 @@ def predecir_respuesta(pregunta):
 
 # Función para mostrar el menú
 def mostrar_menu():
-    print("Menú de opciones:")
+    menu_text = "Menú de opciones:\n"
     for i, pregunta in enumerate(preguntas, start=1):
-        print(f"{i}. {pregunta}")
-    print("0. Hacer una pregunta libre")
-    print("salir para salir")
+        menu_text += f"{i}. {pregunta}\n"
+    menu_text += "0. Hacer una pregunta libre\n"
+    menu_text += "Elije una opción: \n"
+    return menu_text
+
+def mostrar_respuesta(respuesta):
+    window['respuesta_area'].print("Usuario: " + window['pregunta'].get())
+    window['respuesta_area'].print("Bot: " + respuesta)
+    window['respuesta_area'].print("")  
+    window['respuesta_area'].print(mostrar_menu())  
+    window['pregunta'].update('') 
+
+# Crear la interfaz gráfica
+layout = [
+    [sg.Text('Chatbot Universidad Aconcagua')],
+    [sg.Multiline(mostrar_menu(), key='respuesta_area', size=(120, 30))],
+    [sg.Text("Pregunta: "), sg.InputText(key='pregunta')],
+    [sg.Button("Enviar Pregunta"), sg.Button("Salir")]
+]
+
+window = sg.Window('Chatbot', layout)
 
 # Función para iniciar el chat
 def start_chat():
-    print("¡Hola! Soy tu asistente virtual.")
+    event, values = window.read()
+
+    # window['respuesta_area'].print("¡Hola! Soy tu asistente virtual.")
     while True:
-        mostrar_menu()
-        user_choice = input("Elige una opción: ")
-        if user_choice.lower() == "salir":
-            print("¡Hasta luego!")
+        event, values = window.read()
+        
+        if event in (sg.WIN_CLOSED, 'Salir'):
             break
-        elif user_choice.isdigit() and 0 <= int(user_choice) <= len(preguntas):
-            if int(user_choice) == 0:
-                user_input = input("Hazme una pregunta: ")
-                respuesta_predicha = predecir_respuesta(user_input)
-                print(f"Bot: {respuesta_predicha}")
-            else:
-                respuesta_predicha = respuestas[int(user_choice) - 1]
-                print(f"Bot: {respuesta_predicha}")
+        elif event == 'Enviar Pregunta':
+            user_choice = values['pregunta']
+        if user_choice.isdigit() and int(user_choice) == 0:
+            window['respuesta_area'].print("Bot: Hazme una pregunta")
+            window['pregunta'].update('')
+            # window['respuesta_area'].print(mostrar_menu())
+            while True:
+                event, values = window.read()
+                if event in (sg.WIN_CLOSED, 'Salir'):
+                    break
+                elif event == 'Enviar Pregunta':
+                    user_choice = values['pregunta']
+                    if user_choice:
+                        respuesta_predicha = predecir_respuesta(user_choice)
+                        mostrar_respuesta(respuesta_predicha)
+                        break
+                window['respuesta_area'].print("Esperando a que ingreses una pregunta...")
+
+        elif user_choice.isdigit() and 0 < int(user_choice) <= len(preguntas):
+            respuesta_predicha = respuestas[int(user_choice) - 1]
+            mostrar_respuesta(respuesta_predicha)
         else:
-            print("Opción no válida. Inténtalo de nuevo.")
+            respuesta_predicha = predecir_respuesta(user_choice)
+            mostrar_respuesta(respuesta_predicha)
 
 # Iniciar el chat
 start_chat()
